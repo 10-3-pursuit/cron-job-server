b692c6a3e34338b722358b6fc9190318ed9afbb6
jd richards <jdrichardstech@gmail.com>
2024-03-27 19:38:23 -0400

    cron

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
+const cron = require('node-cron')
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
+cron.schedule('*/10 * * * *', () => {
+  console.log('running a task every 10 minutes')
+})
 // ROUTES
-app.get("/", (req, res) => {
-  res.send("Welcome to Bookmarks App");
-});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Bookmarks App')
+})
 
 // 404 PAGE
-app.get("*", (req, res) => {
-  res.json({ error: "Page not found" });
-});
+app.get('*', (req, res) => {
+  res.json({ error: 'Page not found' })
+})
 // EXPORT
-module.exports = app;
+module.exports = app
